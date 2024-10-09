727813c5594cffd878386474e0d5f466e4e92082
michaellgans <michaellgans@Victoria>
2024-10-09 14:53:16 -0500

    MLG - 2:52 PM - Corrects task 5

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -4,5 +4,5 @@ void main(List<String> args) {
   String input = args[0];
   int grade = int.parse(input);
   assert(grade >= 80, 'Assertion failed: "The score must be bigger or equal to 80"');
-  print(input);
+  print("You Passed");
 }
