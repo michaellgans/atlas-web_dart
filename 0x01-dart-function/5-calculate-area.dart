af284118935f348831df8abd7b63abd5821f0245
michaellgans <michaellgans@Victoria>
2024-10-22 13:02:22 -0500

    MLG - 1:02 PM - creates task 5

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,7 @@
+// Task 5 - Calculate Area
+
+double calculateArea(double height, double base) {
+  String area = (.5 * base * height).toStringAsFixed(2);
+  double calculatedArea = double.parse(area);
+  return calculatedArea;
+}
