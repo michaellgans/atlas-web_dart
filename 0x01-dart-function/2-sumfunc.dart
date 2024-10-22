54a116be6a71713e092c526cae3f60d84a1940c9
michaellgans <michaellgans@Victoria>
2024-10-22 11:45:42 -0500

    MLG - 11:45 AM - creates task 3

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,15 @@
+// Task 2 - Add/Sub
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  int addResult = add(a, b);
+  int subResult = sub(a, b);
+  return ("Add $a + $b = $addResult" + "\n" + "Sub $a + $b = $subResult");
+}
