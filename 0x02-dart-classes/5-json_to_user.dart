c0ba08a479293be7479526f6b76d651e39c9eaf6
michaellgans <michaellgans@Victoria>
2024-10-23 14:21:09 -0500

    MLG - Creates task 5

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,45 @@
+// Task 5 - JSON to User
+
+class User {
+  // Properties
+  int id;
+  String name;
+  int age;
+  double height;
+
+  // Constructor
+  User({
+    required this.id,
+    this.name = "",
+    this.age = 0,
+    this.height = 0.0,
+  });
+
+  // Methods
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height
+    };
+  }
+
+  @override
+  String toString() {
+    return "User(id: $id, name: $name, age: $age, height: $height)";
+  }
+
+  String showName() {
+    return "Hello $name";
+  }
+}
