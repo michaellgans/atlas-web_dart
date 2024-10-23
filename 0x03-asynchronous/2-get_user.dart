731eec2e6d5d8a81d75a5ea7f18d43aa8acacdec
michaellgans <michaellgans@Victoria>
2024-10-23 18:43:47 -0500

    MLG - 6:43 PM - Creates task 2

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,13 @@
+// Task 2 - Get User
+
+// Imports
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    String userData = await fetchUser();
+    print(userData);
+  } catch (error) {
+    print("error caught: $error");
+  }
+}
