bcb26d59d1c9c5861aeb6583a12a49fdea51f3c9
michaellgans <michaellgans@Victoria>
2024-10-23 19:05:27 -0500

    MLG - 7:05 PM - Creates task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,36 @@
+// Task 3 - Greet User
+
+// Imports
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String? userData = await fetchUserData();
+    return userData;
+  } catch (error) {
+    return "error caught: $error";
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool access = await checkCredentials();
+    if (access) {
+      // Fetch the entire JSON object
+      String greeting = await greetUser();
+
+      // Turn JSON into a MAP
+      Map<String, dynamic> userInfo = jsonDecode(greeting);
+
+      // Return just the ID
+      String username = userInfo['username'];
+
+      return "There is a user: true" + "\n" + "Hello $username";
+    } else {
+      return "There is a user: false\nWrong credentials";
+    }
+  } catch (error) {
+    return "error caught: $error";
+  }
+}
